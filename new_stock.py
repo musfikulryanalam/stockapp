3f410144caa063f6432fc39b72ca6a56d939128f
Ryan Alam <ryan.alam@target.com>
2020-09-24 14:28:01 -0700

    Add Arge parse and Docstrings

--- new_stock.py
+++ new_stock.py
@@ -2,19 +2,47 @@
 import pandas_datareader as data
 import pandas as pd
 from datetime import datetime
+import argparse 
 
-Start = '2020-08-03'
+start = '2020-08-03'
 end =  '2020-08-03'
 
 stock = "AMZN"
 
+parser = argparse.ArgumentParser(description= 'Calculate and Buy stock')
+parser.add_argument('-t', '--stock', type=str, metavar='', help='Ticker for stock to buy' )
+parser.add_argument('-s', '--start', type=str, const=1, default=str(datetime.now().strftime('%Y-%m-%d')), nargs='?', metavar='', help='Start date' )
+parser.add_argument('-e','--end', type=str, const=1, default=str(datetime.now().strftime('%Y-%m-%d')), nargs='?', metavar='', help='end date' )
+args = parser.parse_args()
+
+
 def clean_data(stock_data, col):
-    weekdays = pd.date_range(start=Start, end=end)
+    """This Function will take the stock data from yahoo finance and return the ADJ Close number
+
+    Args:
+        stock_data : This is the data for the given stock 
+        col ([type]): The ADJ Close is a column so this will need to be provided too
+
+    Returns:
+        The ADJ Close for the stock
+    """    
+    weekdays = pd.date_range(start=start, end=end)
     clean_data = stock_data[col].reindex(weekdays)
     return clean_data.fillna(method='ffill')
 
-def get_data(ticker, current):
-    stock_data = data.DataReader(stock, 'yahoo', Start, end)
+def get_data(stock, start, end, current):
+    """This Function is to return the stock and calculate the shares and current balance
+
+    Args:
+        stock : The ticker for given stock
+        start : Start date of purchase
+        end   : End date of purchase
+        current : Current money before the purchase
+
+    Returns:
+        [type]: Will return total balance after purchase of stock
+    """    
+    stock_data = data.DataReader(stock, 'yahoo', start, end)
     
     adj_close = clean_data(stock_data, 'Adj Close')
 
@@ -33,6 +61,6 @@ def get_data(ticker, current):
 
 
 current = 10000
-stock_data = get_data(stock,current)
+stock_data = get_data(args.stock,args.start, args.end, current)
 print(stock_data)
 
